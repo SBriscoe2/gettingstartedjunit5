5fc3e00ad2c4d02cdecc5de4944fedb6c86fe9e3
SBriscoe2 <shabirs@tiscali.co.uk>
2019-03-18 13:55:43 +0000

    Tests passing - refactored to include nested annotation to group test

--- test/patientintake/DateTimeConverterTest.java
+++ test/patientintake/DateTimeConverterTest.java
@@ -1,43 +1,69 @@
 package patientintake;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@DisplayName("DateTimeConverter should")
 class DateTimeConverterTest {
 
-    @Test
-    void convertsTodayStringCorrectly() {
-        System.out.println("Converts Today String");
-        LocalDateTime result = DateTimeConverter.convertsStringToDateTime("today 1:00 pm",
-                LocalDate.of(2019, 03, 18));
-        assertEquals(result, LocalDateTime.of(2019, 03, 18, 13, 0));
+    @Nested
+    // used to group tests together. original display names commented out
+    @DisplayName("Converts string with 'today' keyword")
+    class TodayTests {
+
+        @Test
+        @DisplayName("correctly")
+                //@DisplayName(Converts strig with today keyword correctly)
+            // using Lamda to produce error msg if failure only reduces processing time of msg
+        void convertsTodayStringCorrectly() {
+            LocalDate today = LocalDate.of(2019, 03, 18);
+            LocalDateTime result = DateTimeConverter.convertsStringToDateTime("today 1:00 pm",
+                    LocalDate.of(2019, 03, 18));
+            assertEquals(result, LocalDateTime.of(2019, 03, 18, 13, 0),
+                    () -> "Failed to convert 'today' string to expected time, today passed was: " + today);
+        }
+
+        @Test
+        @DisplayName("correctly regardless of case")
+                //@@DisplayName("Converts string with 'today' keyword correctly regardless of case")
+            // constantly process error msg when test case run increases processing time of msg
+        void convertsTodayStringCorrectlyCaseInsensitive() {
+            LocalDate today = LocalDate.of(2019, 03, 18);
+            LocalDateTime result = DateTimeConverter.convertsStringToDateTime("ToDay 1:00 pm",
+                    LocalDate.of(2019, 03, 18));
+            assertEquals(result, LocalDateTime.of(2019, 03, 18, 13, 0),
+                    "Failed to convert 'today' string to expected time, today passed was: " + today);
+        }
     }
 
     @Test
+    @DisplayName("Converts pattern to Date time correctly")
     void convertsCorrectPatternToDateTime() {
-        System.out.println("Converts pattern to Date time");
         LocalDateTime result = DateTimeConverter.convertsStringToDateTime("03/18/2019 1:00 pm",
                 LocalDate.of(2019, 03, 18));
         assertEquals(result, LocalDateTime.of(2019, 03, 18, 13, 0));
     }
 
     @Test
+    @DisplayName("Throws exception if incorrect pattern provided")
     void throwExceptionIfIncorrectPatternProvided() {
-        System.out.println("Throws exception if incorrect pattern provided");
         assertThrows(RuntimeException.class, () ->
                 DateTimeConverter.convertsStringToDateTime("03/18/2019 100 pm",
                         LocalDate.of(2019, 03, 18)));
     }
 
     @Test
+    @DisplayName("Throws explicit exception if incorrect pattern provided")
     void throwExceptionIfIncorrectPatternProvidedExplicitException() {
         // assert throws returns a value, the exception is caught, which can be assigned to a local variable
         // then add assertequals to verify the text message is what is expected
-        System.out.println("Throws explicit exception if incorrect pattern provided");
         Throwable error = assertThrows(RuntimeException.class, () ->
                 DateTimeConverter.convertsStringToDateTime("03/18/2019 100 pm",
                         LocalDate.of(2019, 03, 18)));
